pantherman594/yalexs_ble_component

Use PEP 695 type alias for ConfigEntry types (#117632)

custom_components/yalexs_ble/__init__.py
@@ -29,7 +29,7 @@ from .const import (
 from .models import YaleXSBLEData
 from .util import async_find_existing_service_info, bluetooth_callback_matcher
 
-YALEXSBLEConfigEntry = ConfigEntry[YaleXSBLEData]
+type YALEXSBLEConfigEntry = ConfigEntry[YaleXSBLEData]
 
 
 PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.LOCK, Platform.SENSOR]

tinkerborg/apple_tv

__init__.py
@@ -73,7 +73,7 @@ DEVICE_EXCEPTIONS = (
     exceptions.DeviceIdMissingError,
 )
 
-AppleTvConfigEntry = ConfigEntry["AppleTVManager"]
+type AppleTvConfigEntry = ConfigEntry[AppleTVManager]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: AppleTvConfigEntry) -> bool: